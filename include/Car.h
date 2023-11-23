774471e13ab5cb3fd3421503eb41868b50a5c1d5
Maikol Guzmán Alán <mikeguzman@gmail.com>
2023-11-22 23:02:00 -0600

    Important change

--- include/Car.h
+++ include/Car.h
@@ -6,8 +6,12 @@
 #include <string>
 using namespace std;
 
-/*
- *
+
+/*!
+ * \class Car
+ * \brief The class Car is the base class
+ * \file Car.h
+ * \brief The file Car.h is the header file of the class Car
  */
 class Car {
 public:
